6cf1cd5319bbc84385d37a75d9d8d013701dac94
Inhee <sosang92@gmail.com>
2014-05-13 18:18:04 +0900

    MemberEntryController(회원가입)-컨트롤러 작성 log4j.properties, messages.properties 작성 utils/WebConstants 파일 작성

--- src/logic/Shop.java
+++ src/logic/Shop.java
@@ -1,12 +1,13 @@
 package logic;
 
+import java.util.List;
+
 public interface Shop {
-	Cart getCart();
-	Integer calculate TotalAmount(List<Item> itemList);
-	void checkout(Member_VO member, Cart cart);
-	List<Item> getItemList();
-	Item getItemByItemId(Integer itemId);
+	Cart_VO getCart();
+	Integer calculateTotalAmount(List<Item_VO> itemList);
+	void checkout(Member_VO member, Cart_VO cart);
+	List<Item_VO> getItemList();
+	Item_VO getItemByItemId(Integer itemId);
 	Member_VO getMemberByUserEmailAndUserPasswd(String userEmail, String userPasswd);
 	void entryMember(Member_VO member);
-
 }
