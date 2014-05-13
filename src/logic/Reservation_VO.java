d2b497aa7789c0f3358d1c6ab09aa3f176618eba
github chldongsun <github>
2014-05-13 17:33:59 +0900

    VO 클래스의 _,-를 전부 대소문자로 구별하도록 변경

--- src/logic/Reservation_VO.java
+++ src/logic/Reservation_VO.java
@@ -4,73 +4,130 @@ import java.util.Date;
 
 public class Reservation_VO {
 
-	private Member_VO _user_email;
-	private Experience_VO _exp_no;
-	private int _res_table_num;
-	private Date _res_time;
-	private Date _res_canceltime;
-	private int _res_current_applicant;
-	private int _res_max_applicant;
-	private int _res_flag;
-	private String _res_index_detail;
+	private Member_VO userEmail;
+	private Experience_VO expNo;
+	private int resTableNum;
+	private Date resTime;
+	private Date resCanceltime;
+	private int resCurrentApplicant;
+	private int resMaxApplicant;
+	private int resFlag;
+	private String resIndexDetail;
 	
 	
 	
-	public Member_VO get_user_email() {
-		return _user_email;
+	public Reservation_VO() {
+		super();
 	}
-	public void set_user_email(Member_VO _user_email) {
-		this._user_email = _user_email;
+
+
+
+	public Member_VO getUserEmail() {
+		return userEmail;
 	}
-	public Experience_VO get_exp_no() {
-		return _exp_no;
+
+
+
+	public void setUserEmail(Member_VO userEmail) {
+		this.userEmail = userEmail;
 	}
-	public void set_exp_no(Experience_VO _exp_no) {
-		this._exp_no = _exp_no;
+
+
+
+	public Experience_VO getExpNo() {
+		return expNo;
 	}
-	public int get_res_table_num() {
-		return _res_table_num;
+
+
+
+	public void setExpNo(Experience_VO expNo) {
+		this.expNo = expNo;
 	}
-	public void set_res_table_num(int _res_table_num) {
-		this._res_table_num = _res_table_num;
+
+
+
+	public int getResTableNum() {
+		return resTableNum;
 	}
-	public Date get_res_time() {
-		return _res_time;
+
+
+
+	public void setResTableNum(int resTableNum) {
+		this.resTableNum = resTableNum;
 	}
-	public void set_res_time(Date _res_time) {
-		this._res_time = _res_time;
+
+
+
+	public Date getResTime() {
+		return resTime;
 	}
-	public Date get_res_canceltime() {
-		return _res_canceltime;
+
+
+
+	public void setResTime(Date resTime) {
+		this.resTime = resTime;
 	}
-	public void set_res_canceltime(Date _res_canceltime) {
-		this._res_canceltime = _res_canceltime;
+
+
+
+	public Date getResCanceltime() {
+		return resCanceltime;
 	}
-	public int get_res_current_applicant() {
-		return _res_current_applicant;
+
+
+
+	public void setResCanceltime(Date resCanceltime) {
+		this.resCanceltime = resCanceltime;
 	}
-	public void set_res_current_applicant(int _res_current_applicant) {
-		this._res_current_applicant = _res_current_applicant;
+
+
+
+	public int getResCurrentApplicant() {
+		return resCurrentApplicant;
+	}
+
+
+
+	public void setResCurrentApplicant(int resCurrentApplicant) {
+		this.resCurrentApplicant = resCurrentApplicant;
+	}
+
+
+
+	public int getResMaxApplicant() {
+		return resMaxApplicant;
+	}
+
+
+
+	public void setResMaxApplicant(int resMaxApplicant) {
+		this.resMaxApplicant = resMaxApplicant;
+	}
+
+
+
+	public int getResFlag() {
+		return resFlag;
+	}
+
+
+
+	public void setResFlag(int resFlag) {
+		this.resFlag = resFlag;
+	}
+
+
+
+	public String getResIndexDetail() {
+		return resIndexDetail;
+	}
+
+
+
+	public void setResIndexDetail(String resIndexDetail) {
+		this.resIndexDetail = resIndexDetail;
 	}
 	
-	public int get_res_max_applicant() {
-		return _res_max_applicant;
-	}
-	public void set_res_max_applicant(int _res_max_applicant) {
-		this._res_max_applicant = _res_max_applicant;
-	}
-	public int get_res_flag() {
-		return _res_flag;
-	}
-	public void set_res_flag(int _res_flag) {
-		this._res_flag = _res_flag;
-	}
-	public String get_res_index_detail() {
-		return _res_index_detail;
-	}
-	public void set_res_index_detail(String _res_index_detail) {
-		this._res_index_detail = _res_index_detail;
-	}
 	
 	
 	
